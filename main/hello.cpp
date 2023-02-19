0eb2c3ce4eed9dbfa15ee6d60752e0aee65df796
Shreyash Chatterjee <shreyashchatterjee@Shreyashs-MacBook-Air.local>
2023-02-19 17:41:16 +0530

    Added semi-colon

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "This is PES1UG20CS410\n"
+  cout << "This is PES1UG20CS410\n";
   return 0;
 }
